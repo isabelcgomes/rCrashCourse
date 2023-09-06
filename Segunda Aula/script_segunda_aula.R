62a95de3b0208ad4dcef80dcc878e227c7618fde
unknown <isabel.gomes2013@outlook.com>
2023-09-06 20:24:35 -0300

    boxplot

--- Segunda Aula/script_segunda_aula.R
+++ Segunda Aula/script_segunda_aula.R
@@ -20,4 +20,10 @@ shapiro.test(dados$Grau_de_Instruçao_num)
 
 #Teste T para uma amostra
 t.test(dados$Altura, mu = 167)
+# por padrão é bicaudal, para analisar uma das caudas, é necessário adicionar o parâmetro: alternative com um dos valores ("greater" ou "less")
+t.test(dados$Altura, mu = 167, alternative = "greater") #avalia se a média amostral é maior que a média testada
 
+
+
+#Boxplot: gráfico que permite a visualização da média, da distribuição dos dados e do desvio da minha amostra, bem como outliers
+boxplot(dados$Altura, ylab = "Altura (cm)")
